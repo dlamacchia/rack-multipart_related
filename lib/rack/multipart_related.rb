80257b5c3621f6107c2d56afb39ccd07cfe0d439
Brian Moseley <bcm@maz.org>
2012-05-30 02:34:09 -0700

    use rack request, trim param values, fix method name spelling

--- lib/rack/multipart_related.rb
+++ lib/rack/multipart_related.rb
@@ -9,22 +9,19 @@ module Rack
     end
 
     def call(env)
-      content_type = env['CONTENT_TYPE']
+      req = Rack::Request.new(env)
 
-      if content_type =~ /^multipart\/related/ni
-
-        start_part = content_type[/.* start=(?:"((?:\\.|[^\"])*)"|([^;\s]*))/ni] && ($1 || $2)
-        start_part_type = content_type[/.* type=(?:"((?:\\.|[^\"])*)"|([^;\s]*))/ni] && ($1 || $2)
+      if req.media_type == 'multipart/related'
+        start_part = trim_parameter_value(req.media_type_params['start'])
+        start_part_type = trim_parameter_value(req.media_type_params['type'])
 
         if start_part_type == "application/json"
-
-          params = env["rack.request.form_hash"]
-          start_part_attribute = get_attribute(params, start_part)
+          start_part_attribute = get_attribute(req.params, start_part)
           json_data = ::JSON.parse(start_part_attribute[:tempfile].read)
           start_part_attribute[:tempfile].rewind
           env['START_CONTENT_TYPE'] ||= start_part_attribute[:type]
 
-          new_params = handle_atributtes_with_part_refs(json_data, params)
+          new_params = handle_attributes__with_part_refs(json_data, req.params)
           env["rack.request.form_hash"] = new_params
         end
       end
@@ -45,7 +42,7 @@ module Rack
       end
     end
 
-    def handle_atributtes_with_part_refs(data, original_params)
+    def handle_attributes__with_part_refs(data, original_params)
 
       if data.kind_of?(String)
         part_ref = data[/^cid:(.+)$/ni, 1]
@@ -55,15 +52,19 @@ module Rack
         end
       elsif data.kind_of?(Array)
         data.each_with_index do |value, index|
-          data[index] = handle_atributtes_with_part_refs(value, original_params)
+          data[index] = handle_attributes__with_part_refs(value, original_params)
         end
       elsif data.kind_of?(Hash)
         data.each do |key, value|
-          data[key] = handle_atributtes_with_part_refs(value, original_params)
+          data[key] = handle_attributes__with_part_refs(value, original_params)
         end
       end
 
       data
+    end
+
+    def trim_parameter_value(raw)
+      raw && raw.gsub(/^['"]/, '').gsub(/['"]$/, '')
     end
   end
 end
